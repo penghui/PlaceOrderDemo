40a44fb3a09fcbdc4759edb0d5488d537b3dbb78
penghui <penghui@gmail.com>
2016-09-09 19:20:58 +0800

    上传基础代码

--- PlaceOrder/ViewController.swift
+++ PlaceOrder/ViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var countTextField: UITextField!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var subtotalLabel: UILabel!
+    @IBOutlet private weak var totalLabel: UILabel!
+    
+    private var price = 118
+    private var coupon = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
